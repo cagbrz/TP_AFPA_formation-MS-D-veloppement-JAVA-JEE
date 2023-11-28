e3a636a26fd6a863ed8a71bad93598f3f8da61e8
cagbrz <146918706+cagbrz@users.noreply.github.com>
2023-11-28 14:45:57 +0100

    Update Convoi.java

--- Convoi.java
+++ Convoi.java
@@ -1,21 +1,27 @@
 package tp4_bis;
 
 public class Convoi {
-	int taille = 5;
-	Vehicule[] tabConvoi = new Vehicule[taille];
-	TypeVehicule typeVehicule;
-	int indice = 0;
+	Vehicule[]	tabConvoi 	= 	new Vehicule[taille];
+	int		taille		=	5;
+	int 		indice 		= 	0;
+	TypeVehicule 	typeVehicule;
 		
 	public Convoi(int newTaille) {
-		this.taille = newTaille;
-		this.indice = 0;
+		this.taille	=	newTaille;
+		this.indice	= 	0;
 	}
-	
+
+	/**
+ 	* Ajoute au convoi un véhicule
+  	**/
 	public void ajouteAuConvoi(Vehicule vehicule) {
 		this.tabConvoi[indice] = vehicule;
 		indice ++;
 	}
 
+	/**
+ 	* Affiche les différents véhicules présents dans le convoi
+  	**/
 	public void afficheConvoi() {
 		for(int i = 0; i < tabConvoi.length; i++) {
 			if (tabConvoi[i]== null) {
@@ -23,6 +29,8 @@ public class Convoi {
 				break;
 			}
 		}
+		
+		//affiche les informations de chaque véhicule du convoi
 		for(int i = 0; i < tabConvoi.length; i++) {
 			if (i < indice) {
 				System.out.println("---------------------------Véhicule n°"+(i+1)+"---------------------------");
@@ -30,14 +38,20 @@ public class Convoi {
 			}
 		}
 	}
-	
+
+	/**
+ 	* Retourne la vitesse maximale du convoi
+  	* Elle correspond à la vitesse maximale du véhicule le plus petit/léger
+ 	**/
 	public int vitesseMaxConvoi() {
-		int vitesseMaxConvoi = tabConvoi[0].VitesseMax();
+		int vitesseMaxConvoi	=	tabConvoi[0].VitesseMax();
+		
 		for(int i = 1; i < this.indice; i++) {
 			if (tabConvoi[i -1].VitesseMax() <= tabConvoi[i].VitesseMax()) {
 				vitesseMaxConvoi = tabConvoi[i-1].VitesseMax();
 			}
 		}
+		
 		return vitesseMaxConvoi;
 	}
 }
