ef99bb67f34977f5710e577c8468dc413e020755
cagbrz <146918706+cagbrz@users.noreply.github.com>
2023-12-19 09:20:52 +0100

    Update Fleur.java

--- Fleur.java
+++ Fleur.java
@@ -1,37 +1,41 @@
 package tp5;
 
 public class Fleur {
-	TypesFleurs fleur;
-	int qteFleurEnStock;
-	float prixFleur;
+	TypesFleurs	fleur;
+	int		qteFleurEnStock;
+	float		prixFleur;
 	
 	public Fleur(TypesFleurs newfleur, int quantité, float nouveauPrixFleur) {
-		this.fleur = newfleur;
-		this.prixFleur = nouveauPrixFleur;
-		this.qteFleurEnStock = quantité;
+		this.fleur		=	newfleur;
+		this.prixFleur		=	nouveauPrixFleur;
+		this.qteFleurEnStock	=	quantité;
 	}
 	
-	public TypesFleurs getFleur() {return fleur;}
-	public int getQteFleur() {return qteFleurEnStock;}
-	public void setQteFleur(int qteFleur) {this.qteFleurEnStock = qteFleur;}
-	public float getPrixFleur() {return prixFleur;}
+	public	TypesFleurs	getFleur() {return fleur;}
+	public	int		getQteFleur() {return qteFleurEnStock;}
+	public	float		getPrixFleur() {return prixFleur;}
+	public	void		setQteFleur(int qteFleur) {this.qteFleurEnStock = qteFleur;}
 
 	public int ajoutQteFleur(int quantite) {
-		this.qteFleurEnStock += quantite;
+		this.qteFleurEnStock	+=	quantite;
 		return qteFleurEnStock;
 	}
 	
 	public void CasParticulierRetraitQuantite(int quantite) {
-		int count = 0;
-		int qte;
-		int qteFinaleRetire;
-		qte = this.qteFleurEnStock -= quantite;
+		int	count	=	0;
+		int	qte;
+		int	qteFinaleRetire;
+		
+		qte	=	this.qteFleurEnStock -= quantite;
+		
 		while (qte !=0) {
 			count ++;
 			qte ++;
 		}
-		qteFinaleRetire = quantite - count;
-		this.qteFleurEnStock += count;
+		
+		qteFinaleRetire		=	quantite - count;
+		this.qteFleurEnStock	+=	count;
+		
 		System.out.println("La quantité demandée était supérieur au stock de tiges présent pour cette fleur.");
 		System.out.println("Nous avons donc retirer le maximum de tiges possibles, c'est-à-dire "+qteFinaleRetire+" tiges.");
 	}
@@ -52,7 +56,9 @@ public class Fleur {
 
 	@Override
 	public String toString() {
-		return "\nFleur: " + fleur + "\nQuantite de Fleurs : " + qteFleurEnStock + "\nPrix d'une fleur: " + prixFleur+"\n";
+		return "\nFleur: " + fleur 
+			+ "\nQuantite de Fleurs : " + qteFleurEnStock 
+			+ "\nPrix d'une fleur: " + prixFleur+"\n";
 	}
 	
 	
